51f89977fadd2468129355cf858b4a6c9c6a0778
WYQ <1150344015@qq.com>
2025-05-25 15:49:00 +0800

    hack

---
+++ src/student1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+struct Sensor {
+    double L, R, t;
+};
+
+int main() {
+    ifstream fin("./doc/input");
+    int T;
+    // cin >> T; 
+    fin >> T;
+    while (T--) {
+        double D;
+        int n;
+        // cin >> D >> n;  
+        fin >> D >> n;
+
+        vector<Sensor> sensors(n);
+        for (int i = 0; i < n; i++) {
+            // cin >> sensors[i].L >> sensors[i].R >> sensors[i].t;
+            fin >> sensors[i].L >> sensors[i].R >> sensors[i].t;
+        }
+
+        vector<double> positions, speeds;
+
+        positions.push_back(sensors[0].R);
+        speeds.push_back(sensors[0].R / sensors[0].t);
+
+        for (int i = 1; i < n; i++) {
+            double dist = sensors[i].L - sensors[i - 1].R;
+            double speed = dist / sensors[i].t;
+            positions.push_back(sensors[i].R);
+            speeds.push_back(speed);
+        }
+
+        double dist = D - sensors[n - 1].R;
+        double speed = dist / sensors[n - 1].t;
+        positions.push_back(D);
+        speeds.push_back(speed);
+
+        cout << positions.size() << endl;
+        for (double pos : positions) {
+            cout << fixed << setprecision(6) << pos << " ";
+        }
+        cout << endl;
+        for (double spd : speeds) {
+            cout << fixed << setprecision(6) << spd << " ";
+        }
+        cout << endl;
+    }
+    fin.close();
+    return 0;
+}
